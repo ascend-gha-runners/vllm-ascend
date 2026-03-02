ca5bee91ee161678f58eb0c87f11ae5241b1509f
tfhddd <2272751277@qq.com>
2026-03-02 17:11:46 +0800

    feat: 测试vllm test

--- csrc/cmake/config.cmake
+++ csrc/cmake/config.cmake
@@ -210,7 +210,7 @@ if (BUILD_OPEN_PROJECT)
                 --op_debug_config ${OP_DEBUG_CONFIG}
                 --ascend-op-name "${ASCEND_OP_NAME}"
                 --enable-ccache ${ENABLE_CCACHE}
-                --ccache-program ${CCACHE_PROGRAM}
+                --ccache-program ${CUSTOM_CCACHE}
                 RESULT_VARIABLE result
                 OUTPUT_STRIP_TRAILING_WHITESPACE
                 OUTPUT_VARIABLE PREPARE_BUILD_OUTPUT_VARIABLE)
